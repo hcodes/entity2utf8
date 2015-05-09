5ebd7f6c3475c7d56758c46b76f2eb7b323d0722
Denis Seleznev <hcodes@yandex.ru>
2015-05-09 11:04:08 +0300

    Option --exts

--- bin/cli.js
+++ bin/cli.js
@@ -2,10 +2,11 @@
 
 var entity2utf8 = require('../lib/entity2utf8'),
     log = require('../lib/log'),
+    util = require('../lib/utils'),
     program = require('commander'),
     isutf8 = require('isutf8'),
-    Q = require('q'),
-    FS = require('q-io/fs');
+    fs = require('fs'),
+    files = [];
 
 program
     .version(require('../package.json').version)
@@ -16,34 +17,44 @@ program
         function(value) {
             return value.split(',').map(function(item) { return item.trim(); });
         }, [])
+    .option(
+        '--exts <file extensions>',
+        'List of file extensions separated with commas. Example: ".txt,.html"',
+        function(value) {
+            return value.split(',').map(function(item) { return item.trim(); });
+        }, [])
     .parse(process.argv);
 
 if(!program.args.length) {
     program.help();
 }
 
-Q.allSettled(program.args.map(function(file) {
-    return FS.read(file, 'b')
-        .then(function(content) {
-            if(!isutf8(content)) {
-                log.error(file + ' not UTF-8.');
-                return;
-            }
-
-            var result = entity2utf8.find(content.toString('utf8'), program.ignore);
-            if(result.length) {
-                log.errorNewLine(file);
-                log.error('Need replace:');
-
-                result.forEach(function(item) {
-                    log.error(item.original + ' → "' + item.replace + '"' +
-                        (item.count > 1 ? ', count: ' + item.count : ''));
-                });
-            }
-        }, function() {
-            log.errorNewLine(file + ': No such file');
-        });
-})).then(function() {
-    log.hasErrors() && log.log('No errors');
-    process.exit(log.hasErrors() ? 1 : 0);
+program.args.forEach(function(arg) {
+    if(fs.existsSync(arg)) {
+        files = files.concat(util.findFiles(arg, program.exts));
+    } else {
+        log.error(arg + ': is not exists');
+    }
 });
+
+files.forEach(function(file) {
+    var content = fs.readFileSync(file);
+    if(!isutf8(content)) {
+        log.error(file + ' not UTF-8.');
+        return;
+    }
+
+    var result = entity2utf8.find(content.toString('utf8'), program.ignore);
+    if(result.length) {
+        log.errorNewLine(file);
+        log.error('Need replace:');
+
+        result.forEach(function(item) {
+            log.error(item.original + ' → "' + item.replace + '"' +
+                (item.count > 1 ? ', count: ' + item.count : ''));
+        });
+    }
+});
+
+!log.hasErrors() && log.log('No errors.');
+process.exit(log.hasErrors() ? 1 : 0);
