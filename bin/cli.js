e13b55114b5f6ed294d314788b3ac21e81cc230f
Denis Seleznev <hcodes@yandex.ru>
2017-09-19 08:38:45 +0300

    Drop support for old Node.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,15 +1,20 @@
 #!/usr/bin/env node
 
-var entity2utf8 = require('../lib/entity2utf8'),
-    log = require('../lib/log'),
-    util = require('../lib/utils'),
-    program = require('commander'),
-    isutf8 = require('isutf8'),
-    fs = require('fs'),
-    files = [],
-    comma = function(value) {
-        return value.split(',').map(function(item) { return item.trim(); });
-    };
+'use strict';
+
+const fs = require('fs');
+const isutf8 = require('isutf8');
+const program = require('commander');
+
+const entity2utf8 = require('../lib/entity2utf8');
+const log = require('../lib/log');
+const util = require('../lib/utils');
+
+let files = [];
+
+function comma(value) {
+    return value.split(',').map(function(item) { return item.trim(); });
+}
 
 program
     .version(require('../package.json').version)
@@ -18,15 +23,15 @@ program
         'List of file extensions separated with commas. Example: ".txt,.html"', comma, [])
     .option('--ignore <entities>',
         'List of entities separated with commas. Example: "&nbsp;,&raquo;,&shy;"', comma, [])
-    .option('--replace', 'Replace entities to utf-8 symbols in files')
+    .option('--replace', 'Replace entities to UTF-8 symbols in files')
     .parse(process.argv);
 
-if(!program.args.length) {
+if (!program.args.length) {
     program.help();
 }
 
 program.args.forEach(function(arg) {
-    if(fs.existsSync(arg)) {
+    if (fs.existsSync(arg)) {
         files = files.concat(util.findFiles(arg, program.exts));
     } else {
         log.error(arg + ': is not exists');
@@ -34,7 +39,7 @@ program.args.forEach(function(arg) {
 });
 
 files.forEach(function(file) {
-    var content = fs.readFileSync(file);
+    let content = fs.readFileSync(file);
     if(!isutf8(content)) {
         log.error(file + ' not UTF-8.');
         return;
@@ -42,9 +47,9 @@ files.forEach(function(file) {
 
     content = content.toString('utf8');
 
-    var result = entity2utf8.find(content, program.ignore);
-    if(result.length) {
-        if(program.replace) {
+    let result = entity2utf8.find(content, program.ignore);
+    if (result.length) {
+        if (program.replace) {
             result = entity2utf8.replace(content, program.ignore);
             fs.writeFileSync(file, result);
         } else {
@@ -59,7 +64,7 @@ files.forEach(function(file) {
     }
 });
 
-if(!log.hasErrors() && !program.replace) {
+if (!log.hasErrors() && !program.replace) {
     log.log('No errors.');
 }
 
